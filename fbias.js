8d49f8adf6e3e910666945e7b6ef10efb7c29801
raahulkrish <rkraahulkrishna@gmail.com>
2021-02-19 13:45:45 +0530

    feb19 145pm update

--- fbias.js
+++ fbias.js
@@ -1,10 +1,33 @@
 function fbias(asdf) {
     document.getElementById('bnn').innerHTML=asdf;
     var v = asdf;
-    
-    if(v <= 0.7 && v >=0)
+    if(v>0 && v<0.05)
     {
-        document.getElementById('myImg').src= "v07new.gif";
+        document.getElementById('myImg').src= "00.png";
+    }
+    else if(v>=0.05 && v<=0.1)
+    {
+        document.getElementById('myImg').src= "01.png";
+    }
+    else if(v>0.1 && v<=0.15)
+    {
+        document.getElementById('myImg').src= "02.png";
+    }
+    else if(v>0.2 && v<=0.25)
+    {
+        document.getElementById('myImg').src= "03.png";
+    }
+    else if(v>0.3 && v<=0.35)
+    {
+        document.getElementById('myImg').src= "04.png";
+    }
+    else if(v>0.35 && v<=0.4)
+    {
+        document.getElementById('myImg').src= "05.png";
+    }
+    else if(v>0.4 && v<=0.7)
+    {
+        document.getElementById('myImg').src= "06.png";
     }
     else if(v >= 0.75 && v <= 0.9 )
     {
--- myscripts.js
+++ myscripts.js
@@ -4,12 +4,28 @@ function disable() {
 function enable() {
     document.getElementById("s1").disabled = false;
     }
+function timedText1() {
+    setTimeout(myTimeout00, 1800)
+    setTimeout(myTimeout11, 2500) 
+    setTimeout(myTimeout22, 3000) 
+    }    
 function timedText() {
     setTimeout(myTimeout0, 900)
     setTimeout(myTimeout1, 1000) 
     setTimeout(myTimeout2, 1030) 
-    setTimeout(myTimeout3, 1070)
+   setTimeout(myTimeout3, 1070)
     }
+function myTimeout00() {
+    document.getElementById('myImg').src = "1.png";
+    } 
+function myTimeout11() {
+    document.getElementById('myImg').src = "2.png";
+    }
+function myTimeout22() {
+    document.getElementById('myImg').src = "3.png";
+    }
+
+
 function myTimeout0() {
     document.getElementById('myImg').src = "p-n222.png";
     } 
