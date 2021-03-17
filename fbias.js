c0c3d4e7db2e6b46cfce01d26fdf4580cceeb209
Raahul Krishna K B <rkraahulkrishna@gmail.com>
2021-03-17 19:58:40 +0530

    Circuit smoothness

--- fbias.js
+++ fbias.js
@@ -1,33 +1,10 @@
 function fbias(asdf) {
     document.getElementById('bnn').innerHTML=asdf;
     var v = asdf;
-    if(v>0 && v<0.02)
+    
+    if(v>0 && v<=0.53)
     {
-        document.getElementById('myImg').src= "00.png";
-    }
-    else if(v>=0.02 && v<=0.04)
-    {
-        document.getElementById('myImg').src= "01.png";
-    }
-    else if(v>0.04 && v<=0.06)
-    {
-        document.getElementById('myImg').src= "02.png";
-    }
-    else if(v>0.06 && v<=0.08)
-    {
-        document.getElementById('myImg').src= "03.png";
-    }
-    else if(v>0.08 && v<=0.1)
-    {
-        document.getElementById('myImg').src= "04.png";
-    }
-    else if(v>0.1 && v<=0.12)
-    {
-        document.getElementById('myImg').src= "05.png";
-    }
-    else if(v>0.12 && v<=0.53)
-    {
-        document.getElementById('myImg').src= "06.png";
+        document.getElementById('myImg').src= "06.gif";
     }
     else if(v >= 0.54 && v <= 0.58 )
     {
@@ -62,26 +39,6 @@ function fbias(asdf) {
         document.getElementById('myImg').src= "fbiasworkloop9.gif";
     }
     else if(v > 0.77 && v <= 2 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop10.gif";
-    }
-    else if(v > 2.5 && v <= 3)
-    {
-        document.getElementById('myImg').src= "fbiasworkloop6.gif";
-    }
-    else if(v > 3 && v <= 3.5 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop7.gif";
-    }
-    else if(v > 3.5 && v <= 4 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop8.gif";
-    }
-    else if(v > 4 && v <= 4.5 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop9.gif";
-    }
-    else if(v > 4.5 && v <= 5 )
     {
         document.getElementById('myImg').src= "fbiasworkloop10.gif";
     }
--- myscripts.js
+++ myscripts.js
@@ -21,29 +21,33 @@ function timedText() {
     setTimeout(myTimeout1, 1000) 
     setTimeout(myTimeout2, 1030) 
    setTimeout(myTimeout3, 1070)
+   setTimeout(myTimeout4, 6070)
    
     }
 function myTimeout00() {
-    document.getElementById('myImg').src = "1.png";
+    document.getElementById('myImg').src = "1.gif";
     } 
 function myTimeout11() {
-    document.getElementById('myImg').src = "2.png";
+    document.getElementById('myImg').src = "2.gif";
     }
 function myTimeout22() {
-    document.getElementById('myImg').src = "3.png";
+    document.getElementById('myImg').src = "3.gif";
     }
 
 
 function myTimeout0() {
-    document.getElementById('myImg').src = "p-n222.png";
+    document.getElementById('myImg').src = "p-n222.gif";
     } 
 function myTimeout1() {
-    document.getElementById('myImg').src = "p-n2.png";
+    document.getElementById('myImg').src = "p-n2.gif";
     }
 function myTimeout2() {
     document.getElementById('myImg').src = "jnformation112.gif";
     }
 function myTimeout3() {
-    document.getElementById('myImg').src = "depletion_layer111.gif";
-    check=1;
+    document.getElementById('myImg').src = "depletion_layer1112.gif";
     }
+function myTimeout4() {
+    document.getElementById('myImg').src = "Snapshot_1.gif";
+    check=1;
+}
