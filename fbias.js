5c2f9d46d8fcf31bdc7434c5203f1d19f8f76395
Raahul Krishna K B <rkraahulkrishna@gmail.com>
2021-03-17 11:52:20 +0530

    Home commit

--- fbias.js
+++ fbias.js
@@ -1,69 +1,69 @@
 function fbias(asdf) {
     document.getElementById('bnn').innerHTML=asdf;
     var v = asdf;
-    if(v>0 && v<0.05)
+    if(v>0 && v<0.02)
     {
         document.getElementById('myImg').src= "00.png";
     }
-    else if(v>=0.05 && v<=0.1)
+    else if(v>=0.02 && v<=0.04)
     {
         document.getElementById('myImg').src= "01.png";
     }
-    else if(v>0.1 && v<=0.15)
+    else if(v>0.04 && v<=0.06)
     {
         document.getElementById('myImg').src= "02.png";
     }
-    else if(v>0.2 && v<=0.25)
+    else if(v>0.06 && v<=0.08)
     {
         document.getElementById('myImg').src= "03.png";
     }
-    else if(v>0.3 && v<=0.35)
+    else if(v>0.08 && v<=0.1)
     {
         document.getElementById('myImg').src= "04.png";
     }
-    else if(v>0.35 && v<=0.4)
+    else if(v>0.1 && v<=0.12)
     {
         document.getElementById('myImg').src= "05.png";
     }
-    else if(v>0.4 && v<=0.7)
+    else if(v>0.12 && v<=0.53)
     {
         document.getElementById('myImg').src= "06.png";
     }
-    else if(v >= 0.75 && v <= 0.9 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop1.gif";
-    }
-    else if(v > 0.9 && v <= 1.1 )
+    else if(v >= 0.54 && v <= 0.58 )
     {
         document.getElementById('myImg').src= "fbiasworkloop1.5.gif";
     }
-    else if(v > 1.1 && v <= 1.3 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop2.gif";
-    }
-    else if(v > 1.3 && v <= 1.5 )
+    else if(v > 0.58 && v <= 0.6 )
     {
         document.getElementById('myImg').src= "fbiasworkloop2.5.gif";
     }
-    else if(v > 1.5 && v <= 1.7 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop3.gif";
-    }
-    else if(v > 1.7 && v <= 1.9 )
+    else if(v > 0.6 && v <= 0.65 )
     {
         document.getElementById('myImg').src= "fbiasworkloop3.5.gif";
     }
-    else if(v > 1.9 && v <= 2.1 )
-    {
-        document.getElementById('myImg').src= "fbiasworkloop4.gif";
-    }
-    else if(v > 2.1 && v <= 2.3 )
+    else if(v > 0.65 && v <= 0.7 )
     {
         document.getElementById('myImg').src= "fbiasworkloop4.5.gif";
     }
-    else if(v > 2.3 && v <= 2.5 )
+    else if(v > 0.7 && v <= 0.72 )
     {
-        document.getElementById('myImg').src= "fbiasworkloop5.gif";
+        document.getElementById('myImg').src= "fbiasworkloop6.gif";
+    }
+    else if(v > 0.72 && v <= 0.74 )
+    {
+        document.getElementById('myImg').src= "fbiasworkloop7.gif";
+    }
+    else if(v > 0.74 && v <= 0.76 )
+    {
+        document.getElementById('myImg').src= "fbiasworkloop8.gif";
+    }
+    else if(v > 76 && v <= 0.77 )
+    {
+        document.getElementById('myImg').src= "fbiasworkloop9.gif";
+    }
+    else if(v > 0.77 && v <= 2 )
+    {
+        document.getElementById('myImg').src= "fbiasworkloop10.gif";
     }
     else if(v > 2.5 && v <= 3)
     {
